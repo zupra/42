e4634df8519dc1e37b1ed477adabd0fcb5a2709d
Dima Zubkov <dima5447298@gmail.com>
2021-12-15 15:25:49 +0300

    WIP nav

---
+++ pages/app/tariff.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+
+  <pre 
+    style="height:300px"
+    class="p-1 bg-gray-50 text-xs overflow-auto resize-y">{{tariffList}}</pre>
+
+  <a-card class="!mb-6" title="Добавить тариф">
+  </a-card>
+
+
+</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const [tariff] = await Promise.all([
+      $axios.$get('/admin/tariff'),
+    ])
+    return {
+      tariffList: tariff.data,
+    }
+  },
+  data() {
+    return {}
+  },
+
+  methods: {
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/app/user.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+
+  <pre 
+    style="height:68vh"
+    class="p-1 bg-gray-50 text-xs overflow-auto resize-y">{{userList}}</pre>
+
+  <a-card class="!mb-6" title="Добавить Пользователя">
+  </a-card>
+
+
+</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const [user] = await Promise.all([
+      $axios.$get('/admin/user'),
+    ])
+    return {
+      userList: user.data,
+    }
+  },
+  data() {
+    return {}
+  },
+
+  methods: {
+  },
+
+}
+</script>
+
+<style>
+
+</style>
